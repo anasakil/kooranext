f3bd509caa6b5b8b68cc2f4c99913082cb6afa29
anasakil <akil87382@gmail.com>
2024-09-11 23:26:40 +0100

    first  commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['cloudinary-marketing-res.cloudinary.com'],
+      },
+};
 
 export default nextConfig;
