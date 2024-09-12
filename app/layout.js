561a932a80c1b4ae175680cfe887be6e44c99c59
anasakil <96956171+anasakil@users.noreply.github.com>
2024-09-12 03:49:24 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,27 +1,28 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from '../components/Navbar';
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "kooranext", // Mettez à jour le titre ici
-  description: "Welcome to My Awesome Site, where we offer amazing content and insights.", // Mettez à jour la description ici
+  title: "kooranext",
+  description: "Welcome to My Awesome Site, where we offer amazing content and insights.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        {/* Meta tags for SEO */}
-        <meta name="description" content="Welcome to My Awesome Site, where we offer amazing content and insights." />
-        <meta name="keywords" content="awesome, site, content, insights" />
-        <meta name="author" content="Your Name" />
-        
-        {/* Google AdSense verification */}
-        <meta name="google-adsense-account" content="ca-pub-6784576418150334">
-      </head>
       <body className={inter.className}>
+        <Head>
+          {/* Meta tags for SEO */}
+          <meta name="description" content="Welcome to My Awesome Site, where we offer amazing content and insights." />
+          <meta name="keywords" content="awesome, site, content, insights" />
+          <meta name="author" content="Your Name" />
+
+          {/* Google AdSense verification */}
+          <meta name="google-adsense-account" content="ca-pub-6784576418150334" />
+        </Head>
         <Navbar />
         {children}
       </body>
